0498a2ba1e00464c6a9368b735719f3892aff781
mdshourovislam <mdsourovislam10001@gmail.com>
2025-11-05 02:58:02 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import Navbar from './component/Navbar'
 import Home from './Pages/Home'
 import About from './Pages/About'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
 import Contarct from './Pages/Contarct'
 import Layout from './layout/Layout'
+import Notfound from './Pages/Notfound'
 
 const App = () => {
 
   const MyRoute = createBrowserRouter(createRoutesFromElements(
 
     <Route path='/' element={<Layout/>}>
-      <Route index element={<Home/>}/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/contract' element={<Contarct/>}/>
+      <Route path='*' element={<Notfound/>}/>
     </Route>
 
 
---
+++ src/Pages/Notfound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notfound = () => {
+  return (
+    <div className='w-full h-screen  flex items-center justify-center'>
+        <img src="https://i.pinimg.com/originals/e4/30/10/e430101033efff9a294eaafecbac846a.gif" alt="" />
+    </div>
+  )
+}
+
+export default Notfound
